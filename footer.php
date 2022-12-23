1eeac062716a6e30295386d7ba88b4681088e4e7
Rabeet Ahmed <rabeet0846551@gmail.com>
2022-12-24 01:56:19 +0500

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+	<!-- JavaScripts -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/plugins.min.js"></script>
+	<script src="assets/js/common.js"></script>
+	<script src="assets/js/mapbox.init.js"></script>
+
+
+</body>
+</html>
---
+++ resume.php
@@ -0,0 +1,136 @@
+<?php $page = 'resume'; include 'header.php';?>
+
+
+						<!-- About -->
+						<div class="pb-3">
+							<h1 class="title title--h1 title__separate">Resume</h1>
+						</div>
+
+						<!-- Experience -->
+						<h2 class="title title--h2"><span class="box icon-box"><i
+									class="font-icon feathericon-book-open"></i></span>Education</h2>
+						<div class="timeline">
+							<!-- Item -->
+							<article class="timeline__item">
+								<h5 class="title title--h4 timeline__title">Top Land School</h5>
+								<span class="timeline__period">2010 — 2017</span>
+								<p class="timeline__description">Matriculation in Science Group.</p>
+							</article>
+
+							<!-- Item -->
+							<article class="timeline__item">
+								<h5 class="title title--h4 timeline__title">Aptech Learning Institute</h5>
+								<span class="timeline__period">2020 — Continue</span>
+								<p class="timeline__description">Software Engineering Diploma + Intermidiate.</p>
+							</article>
+
+							<!-- Item -->
+							<!-- <article class="timeline__item">
+								<h5 class="title title--h4 timeline__title">Iqra University - [BS'c]</h5>
+								<span class="timeline__period">Coming Soon</span>
+								<p class="timeline__description">I'm Studing in Aptech Learning Institute in Korangi Brand. I do Software Engineering witn Intermidiate Diploma from here.</p>
+							</article> -->
+
+
+						</div>
+
+						<h2 class="title title--h2"><span class="box icon-box"><i
+									class="font-icon feathericon-briefcase"></i></span>Experience</h2>
+						<div class="timeline">
+							<!-- Item -->
+							<article class="timeline__item">
+								<h5 class="title title--h4 timeline__title">Graphics Designing</h5>
+								<span class="timeline__period">5 Years of Expirence</span>
+								<p class="timeline__description">I was maked many project's of Graphics
+									Designing for Client.</p>
+							</article>
+
+							<!-- Item -->
+							<article class="timeline__item">
+								<h5 class="title title--h4 timeline__title">UI UX Design</h5>
+								<span class="timeline__period">2 Year of Expirence</span>
+								<p class="timeline__description">I have designed for many organizations
+									Ex. Logo, Banner, Poser, Animation,
+									Prototype for Android Apps UserInterfaces.</p>
+							</article>
+
+							<!-- Item -->
+							<article class="timeline__item">
+								<h5 class="title title--h4 timeline__title">Web Designer</h5>
+								<span class="timeline__period">2 Year of Expirence at <b> Tech4Minds </b></span>
+								<p class="timeline__description">Created Personal Web Applications for
+									their Business Solution.</p>
+							</article>
+						</div>
+
+						<!-- Skills -->
+						<h2 class="title title--h2 mt-3">My Skills</h2>
+						<div class="box box-inner mb-0">
+							<!-- Web Design -->
+							<div class="progress">
+								<div class="progress-text"><span>Website Designing</span> 90%</div>
+								<div class="progress-bar"><span role="progressbar" aria-valuenow="90" aria-valuemin="0"
+										aria-valuemax="100"></span></div>
+							</div>
+
+							<!-- Web Design -->
+							<div class="progress">
+								<div class="progress-text"><span>Graphic Designing</span> 85%</div>
+								<div class="progress-bar"><span role="progressbar" aria-valuenow="85" aria-valuemin="0"
+										aria-valuemax="100"></span></div>
+							</div>
+
+							
+							<!-- Web Design -->
+							<div class="progress">
+								<div class="progress-text"><span>UI UX</span> 80%</div>
+								<div class="progress-bar"><span role="progressbar" aria-valuenow="80" aria-valuemin="0"
+										aria-valuemax="100"></span></div>
+							</div>
+
+							<!-- Web Design -->
+							<div class="progress">
+								<div class="progress-text"><span>SEO Optimization</span> 70%</div>
+								<div class="progress-bar"><span role="progressbar" aria-valuenow="70" aria-valuemin="0"
+										aria-valuemax="100"></span></div>
+							</div>
+
+							<!-- Web Design -->
+							<div class="progress">
+								<div class="progress-text"><span>Office Automation</span> 60%</div>
+								<div class="progress-bar"><span role="progressbar" aria-valuenow="60" aria-valuemin="0"
+										aria-valuemax="100"></span></div>
+							</div>
+
+							<!-- Web Design -->
+							<div class="progress">
+								<div class="progress-text"><span>Back-End</span> 50%</div>
+								<div class="progress-bar"><span role="progressbar" aria-valuenow="50" aria-valuemin="0"
+										aria-valuemax="100"></span></div>
+							</div>
+
+
+
+							<!-- Web Design -->
+							<div class="progress">
+								<div class="progress-text"><span>Laravel</span> 50%</div>
+								<div class="progress-bar"><span role="progressbar" aria-valuenow="50" aria-valuemin="0"
+										aria-valuemax="100"></span></div>
+							</div>
+
+							<!-- Web Design -->
+							<div class="progress">
+								<div class="progress-text"><span>WordPress</span> 50%</div>
+								<div class="progress-bar"><span role="progressbar" aria-valuenow="50" aria-valuemin="0"
+										aria-valuemax="100"></span></div>
+							</div>
+
+							<a href="./assets/images/Rabeet Resume.pdf" class="btn"><i class="fa-solid fa-cloud-arrow-down"></i>&nbsp;&nbsp;Download Resume</a>
+						</div>
+					</div>
+				</div><!-- Content -->
+			</div><!-- sticky-parent -->
+		</div><!-- container -->
+	</main>
+
+	<?php include 'footer.php';?>
